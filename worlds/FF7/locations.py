bf78562430af95277f95e27a84f571065e948bec
scsblaze <glencole92@gmail.com>
2025-06-20 11:13:31 -0400

    Create locations.py

---
+++ worlds/FF7/locations.py
@@ -0,0 +1,11 @@
+from BaseClasses import Location
+
+# ToDo - Build Location Table
+#location_table = {
+#    "Temple of the Ancients": None,
+#    "Shinra HQ Keycard 60": None,
+#     ...
+}
+
+class FF7Location(Location):
+    game = "Final Fantasy VII"
